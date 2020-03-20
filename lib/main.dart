62870195bc6f4dd960ed1ef7be748701c2860c59
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-20 11:52:04 -0600

    InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_formvalidation/src/bloc/provider.dart';
 import 'package:flutter_formvalidation/src/pages/home_page.dart';
 import 'package:flutter_formvalidation/src/pages/login_page.dart';
 
@@ -7,13 +8,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      initialRoute: 'login',
-      routes: {
-        'login': (BuildContext context) => LoginPage(),
-        'home': (BuildContext context) => HomePage(),
-      }
+    return Provider(
+      child: MaterialApp(
+        title: 'Material App',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage(),
+        }
+      )
     );
   }
 }
