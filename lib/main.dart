8c5efd3f884109c10c64ff38ea25383450c56056
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-20 12:07:51 -0600

    Conectar los inputs con los Streams

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,10 @@ class MyApp extends StatelessWidget {
         routes: {
           'login': (BuildContext context) => LoginPage(),
           'home': (BuildContext context) => HomePage(),
-        }
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple
+        ),
       )
     );
   }
